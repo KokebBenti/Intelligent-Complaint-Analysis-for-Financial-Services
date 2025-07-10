f60ea85f7689fadc72b9467a0a58a24f88d8d7a7
Kokeb <kokebbelete1@gmail.com>
2025-07-10 20:33:35 +0300

    overwrite a file

--- Scripts/RAG.py
+++ Scripts/RAG.py
@@ -4,7 +4,7 @@ def get_user_query():
 
 def load_embedding_model():
     from sentence_transformers import SentenceTransformer
-    model = SentenceTransformer('all-MiniLM-L6-v2', device='cuda')
+    model = SentenceTransformer('all-MiniLM-L6-v2')
     return model
 
 def similarity_search(index, vector, chunks, model, k=5):
@@ -28,7 +28,7 @@ def build_rag_prompt(query,context):
 def llm_usage(final_prompt):
   from langchain.llms import HuggingFacePipeline
   from transformers import pipeline
-  hf_pipeline = pipeline("text-generation", model="tiiuae/falcon-7b-instruct", max_new_tokens=256)
+  hf_pipeline = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", max_new_tokens=256)
   llm = HuggingFacePipeline(pipeline=hf_pipeline)      
   response = llm(final_prompt)
   return response
@@ -43,7 +43,7 @@ def extract_answer(response_text):
 def rerank_chunks(user_question, retrieved_chunks, top_k=5, batch_size=8):
     from tqdm import tqdm
     from sentence_transformers import CrossEncoder
-    reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2", device='cuda')
+    reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
     pairs = [(user_question, chunk) for chunk in retrieved_chunks]
     scores = []
     for i in tqdm(range(0, len(pairs), batch_size), desc="Re-ranking"):
